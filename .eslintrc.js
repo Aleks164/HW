65303bdfdfa6e088980702dac4cfef980ceffb5d
Aleks164 <alekseylehamail.ru>
2021-10-30 10:21:27 +0300

    6step

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "airbnb-base"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    "no-console": "off",
+        "no-restricted-syntax": [
+            "error",
+            {
+                "selector": "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+                "message": "Unexpected property on console object was called"
+            }
+        ],
+        'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
+        "no-alert": "off",
+  },
+  plugins: ['jest'],
 };
