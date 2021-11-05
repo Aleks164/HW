c2e17eed7ea3dd6c1c3e7fe1e26193162988291d
Aleks164 <alekseylehamail.ru>
2021-11-05 20:36:45 +0300

    0 er all test pass_2

--- HW-5-lect/DZ_2/2st ex/2DZ.test.js
+++ HW-5-lect/DZ_2/2st ex/2DZ.test.js
@@ -4,13 +4,17 @@ it("returns 28.10.2021 это четверг", () => {
   jest.spyOn(window, "prompt").mockReturnValue("28.10.2021");
   window.alert = jest.fn();
   calc();
-  expect(window.alert).toHaveBeenCalledWith("28.10.2021 это четверг");
+  expect(window.alert).toHaveBeenCalledWith(
+    "28.10.2021 это четверг" || "28.10.2021 это Thursday"
+  );
 });
 it("returns 28.10.2022 это пятница", () => {
   jest.spyOn(window, "prompt").mockReturnValue("28.10.2022");
   window.alert = jest.fn();
   calc();
-  expect(window.alert).toHaveBeenCalledWith("28.10.2022 это пятница");
+  expect(window.alert).toHaveBeenCalledWith(
+    "28.10.2022 это пятница" || "28.10.2022 это Friday"
+  );
 });
 it("returns 123 это пятница", () => {
   jest.spyOn(window, "prompt").mockReturnValue("123");
